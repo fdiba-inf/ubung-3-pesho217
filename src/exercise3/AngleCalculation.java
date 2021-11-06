9dd5bba790f854041515ac9a4e46cf4a41cd5f86
pesho217 <>
2021-11-06 17:04:22 +0000

    Files updated

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -1,33 +1,28 @@
 package exercise3;
 
 import java.util.Scanner;
-import java.lang.Math;
 
 public class AngleCalculation {
 
-  public static void main(String []args) {
-
-    Scanner input = new Scanner(System.in);
-    
-    double radiant;
-    double grad;
-    char ch;
-
-    do {
-      int winkel = input.nextInt();
-      ch = input.next().charAt(0);
-      if ( ch == 'r' ) {
-        grad = winkel/Math.PI *180;
-        System.out.println("Angle: " + grad + "d" );
-        
-      } else {
-        radiant = winkel*Math.PI/180;
-        System.out.println("Angle: " + radiant + "r" );
-
-
-      } 
-      ch = input.next().charAt(0);
-      
-    }while (ch == 'r' || ch == 'd');
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double angle;
+        char ch;
+        double radians;
+        double degrees;
+        do {
+          angle = input.nextDouble();
+          ch = input.next().charAt(0);
+         if (ch == 'r') {
+           radians = angle;
+           degrees = (180*radians)/Math.PI;
+           System.out.println("Angle: " + degrees + "d");
+         } else if (ch == 'd') {
+           degrees = angle;
+           radians = (Math.PI*degrees)/180;
+           System.out.println("Angle: " + radians + "r");
+         }
+          
+        } while(ch == 'r' || ch == 'd');
+    }
 }
--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -4,37 +4,42 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Triangle {
-  public static void main(String []args) {
-    Scanner input = new Scanner(System.in);
 
-    int a;
-    int b;
-    int c;
-    double p = 0.0;
-    double S = 0.0;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    do {
-      a = input.nextInt();
-      b = input.nextInt();
-      c = input.nextInt();
-      if ((a + b) > c && (a + c) > b && (b + c) > a) {
-        p = (a + b +c)/2;
-        S = Math.sqrt(p*(p - a)*(p - b)*(p - c));
-        System.out.println("Area: " + S);
-        if (a == b && a != c ) {
-          System.out.println("Triangle: isosceles");
-          break;
-        } else if (a == b && c == b && a == c) {
-          System.out.println("Triangle: equilateral");
-          break;
-        } else {
-          System.out.println( "Triangle: scalene");
-          break;
-        } 
-        } else {
-          System.out.println("Values are not correct!");
-          break;
-      } 
-    } while ( a > 0 && b > 0 && c > 0);
-  }
+        double P = 0.0;
+        double S = 0.0;
+        double a;
+        double b;
+        double c;
+
+
+        do{
+           a = input.nextDouble();
+           b = input.nextDouble();
+           c = input.nextDouble();
+          if((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+            System.out.println("Values are not correct!");
+          } else {
+          P = (a + b + c) / 2;
+          S = Math.sqrt(P * (P - a) * (P - b) * (P - c));
+          System.out.println("Area: " + S);
+          if(a == b && a == c) {
+            System.out.println("Triangle: equilateral");
+          } else if(a == b || a == c || b == c) {
+            System.out.println("Triangle: isosceles");
+          } else {
+            System.out.println("Triangle: scalene");
+          }
+          }
+
+        }
+          while(a > 0 && b > 0 && c > 0);
+        
+        
+          
+        
+    }
+
 }
